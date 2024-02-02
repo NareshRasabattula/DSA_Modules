b7bf1500c754acda96b9d7512ce89b6399fd7123
DurgaNaresh <rdurganaresh@gmail.com>
2024-02-02 13:08:20 +0530

    Project Added

---
+++ DSA_Programs/src/main/java/com/durga/Main.java
@@ -0,0 +1,14 @@
+package com.durga;
+
+/**
+ * @author DurgaNaresh
+ * 2/2/2024
+ * Hard work definitely pays off.
+ * There is no substitute of hard work.
+ * There is no shortcut to success.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
